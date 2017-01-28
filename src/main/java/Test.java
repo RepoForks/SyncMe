88305832a0d69b143c619337e4543cd42a16420c
Adrián <arodriba@ull.edu.es>
2017-01-28 16:07:47 +0000

    Initial commit + License

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by @AdrianBZG (www.adrianbazaga.com) on 28/01/2017.
+ */
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("hey");
+    }
+}

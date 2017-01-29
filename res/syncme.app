89360e81216ec167f727c887fdf0131796ccb383
Adrián <arodriba@ull.edu.es>
2017-01-29 16:30:50 +0000

    Some operations testing

---
+++ res/syncme.app
@@ -0,0 +1,4 @@
+{
+  "key": "le647urkyhsv5l4",
+  "secret": "hiwjmiykq2il095"
+}
---
+++ src/main/java/operations/dropbox/GetAccountInfo.java
@@ -0,0 +1,56 @@
+package operations.dropbox;
+
+/**
+ * Created by @AdrianBZG (www.adrianbazaga.com) on 29/01/2017.
+ */
+
+import com.dropbox.core.DbxAuthInfo;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.json.JsonReader;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.users.FullAccount;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class GetAccountInfo
+{
+    public static void main(String[] args)
+            throws IOException
+    {
+        // Only display important log messages.
+        Logger.getLogger("").setLevel(Level.WARNING);
+
+        String argAuthFile = "C:\\Users\\QiCanarias23\\IdeaProjects\\syncme\\syncme.auth";
+
+        // Read auth info file.
+        DbxAuthInfo authInfo;
+        try {
+            authInfo = DbxAuthInfo.Reader.readFromFile(argAuthFile);
+        }
+        catch (JsonReader.FileLoadException ex) {
+            System.err.println("Error loading <auth-file>: " + ex.getMessage());
+            System.exit(1); return;
+        }
+
+        // Create a DbxClientV1, which is what you use to make API calls.
+        DbxRequestConfig requestConfig = new DbxRequestConfig("examples-account-info");
+        DbxClientV2 dbxClient = new DbxClientV2(requestConfig, authInfo.getAccessToken(), authInfo.getHost());
+
+        // Make the /account/info API call.
+        FullAccount dbxAccountInfo;
+        try {
+            dbxAccountInfo = dbxClient.users()
+                    .getCurrentAccount();
+        }
+        catch (DbxException ex) {
+            System.err.println("Error making API call: " + ex.getMessage());
+            System.exit(1); return;
+        }
+
+        System.out.print(dbxAccountInfo.toStringMultiline());
+    }
+}

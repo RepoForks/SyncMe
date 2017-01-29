e9223ed3b6e491da83eb1c133328f4cbd17cdad3
Adrián <arodriba@ull.edu.es>
2017-01-29 20:12:08 +0000

    Initial dropbox window workaround

--- src/main/java/gui/DropboxMainWindow.java
+++ src/main/java/gui/DropboxMainWindow.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
+import static com.sun.org.apache.xalan.internal.utils.SecuritySupport.getResourceAsStream;
 
 /**
  * Created by @AdrianBZG (www.adrianbazaga.com) on 29/01/2017.
@@ -14,14 +17,14 @@ public class DropboxMainWindow extends JFrame {
     public DropboxMainWindow() {
         setTitle("SyncMe: Dropbox Window");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
+        setSize(1024, 800);
         setResizable(false);
 
         setLayout(new BorderLayout());
 
         initializeElements();
 
-        pack();
+        //pack();
     }
 
     public void showWindow() {
@@ -33,16 +36,45 @@ public class DropboxMainWindow extends JFrame {
         JPanel topButtonsPanel = new JPanel(new GridLayout(1,0));
 
         JButton userInfoButton = new JButton();
+        JButton downButton = new JButton();
+        JButton upButton = new JButton();
+        JButton deleteButton = new JButton();
+        JButton createFolderButton = new JButton();
+        JButton configureButton = new JButton();
         try {
-            Image img = ImageIO.read(getClass().getResource("res\\user-identity.png"));
+            Image img = ImageIO.read(new File("res\\user-identity.png"));
             userInfoButton.setIcon(new ImageIcon(img));
+
+            img = ImageIO.read(new File("res\\go-down.png"));
+            downButton.setIcon(new ImageIcon(img));
+
+            img = ImageIO.read(new File("res\\go-up.png"));
+            upButton.setIcon(new ImageIcon(img));
+
+            img = ImageIO.read(new File("res\\delete.png"));
+            deleteButton.setIcon(new ImageIcon(img));
+
+            img = ImageIO.read(new File("res\\folder-blue.png"));
+            createFolderButton.setIcon(new ImageIcon(img));
+
+            img = ImageIO.read(new File("res\\configure.png"));
+            configureButton.setIcon(new ImageIcon(img));
         } catch (Exception ex) {
             System.out.println(ex);
         }
 
         topButtonsPanel.add(userInfoButton);
+        topButtonsPanel.add(downButton);
+        topButtonsPanel.add(upButton);
+        topButtonsPanel.add(deleteButton);
+        topButtonsPanel.add(createFolderButton);
+        topButtonsPanel.add(configureButton);
 
         add(topButtonsPanel, BorderLayout.NORTH);
+
+        add(new JScrollPane(new JLabel("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")), BorderLayout.CENTER);
+
+        add(new JLabel("SyncMe v2 - Copyright 2017 Adrián Rodríguez Bazaga"), BorderLayout.SOUTH);
 
     }
 }

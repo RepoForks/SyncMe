3635a2fc5a89b09f2f6f6ca023c9470d654141c6
Adrián <arodriba@ull.edu.es>
2017-01-29 21:08:59 +0000

    Refactoring

--- src/main/java/operations/dropbox/Indexing.java
+++ src/main/java/operations/dropbox/Indexing.java
@@ -4,19 +4,10 @@ package operations.dropbox;
  * Created by @AdrianBZG (www.adrianbazaga.com) on 29/01/2017.
  */
 import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.files.FileMetadata;
-import com.dropbox.core.v2.files.FolderMetadata;
 import com.dropbox.core.v2.files.ListFolderResult;
 import com.dropbox.core.v2.files.Metadata;
-import com.dropbox.core.v2.users.FullAccount;
 
 import java.util.List;
-
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.IOException;
 
 public class Indexing {
 

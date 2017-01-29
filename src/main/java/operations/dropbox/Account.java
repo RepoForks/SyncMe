344b7a0ce137f83f1019ef02c341e8996ef9a1ff
Adrián <arodriba@ull.edu.es>
2017-01-29 21:19:57 +0000

    First version of account info getters operations finished

--- src/main/java/operations/dropbox/Account.java
+++ src/main/java/operations/dropbox/Account.java
@@ -4,61 +4,56 @@ package operations.dropbox;
  * Created by @AdrianBZG (www.adrianbazaga.com) on 29/01/2017.
  */
 
-import com.dropbox.core.DbxAuthInfo;
 import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.json.JsonReader;
-import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.users.AccountType;
 import com.dropbox.core.v2.users.FullAccount;
-
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class Account
 {
-    public static void main(String[] args)
-            throws IOException
-    {
-        // Only display important log messages.
-        Logger.getLogger("").setLevel(Level.WARNING);
+    private static FullAccount account = null;
 
-        String argAuthFile = "syncme.auth";
-        // Get current account info
-        FullAccount account = null;
+    public static void initialize() {
         try {
             account = CoreManager.getClient().users().getCurrentAccount();
         } catch (DbxException e) {
             e.printStackTrace();
         }
-        System.out.println(account.getName().getDisplayName());
+    }
 
-        // Read auth info file.
-        DbxAuthInfo authInfo;
-        try {
-            authInfo = DbxAuthInfo.Reader.readFromFile(argAuthFile);
-        }
-        catch (JsonReader.FileLoadException ex) {
-            System.err.println("Error loading <auth-file>: " + ex.getMessage());
-            System.exit(1); return;
-        }
+    public static String getAccountName() {
+        if(account == null) initialize();
 
-        // Create a DbxClientV1, which is what you use to make API calls.
-        DbxRequestConfig requestConfig = new DbxRequestConfig("examples-account-info");
-        DbxClientV2 dbxClient = new DbxClientV2(requestConfig, authInfo.getAccessToken(), authInfo.getHost());
+        return account.getName().getDisplayName();
+    }
 
-        // Make the /account/info API call.
-        FullAccount dbxAccountInfo;
-        try {
-            dbxAccountInfo = dbxClient.users()
-                    .getCurrentAccount();
-        }
-        catch (DbxException ex) {
-            System.err.println("Error making API call: " + ex.getMessage());
-            System.exit(1); return;
-        }
+    public static AccountType getAccountType() {
+        if(account == null) initialize();
 
-        System.out.print(dbxAccountInfo.toStringMultiline());
+        return account.getAccountType();
+    }
+
+    public static String getAccountEmail() {
+        if(account == null) initialize();
+
+        return account.getEmail();
+    }
+
+    public static boolean getAccountEmailIsVerified() {
+        if(account == null) initialize();
+
+        return account.getEmailVerified();
+    }
+
+    public static String getAccountCountry() {
+        if(account == null) initialize();
+
+        return account.getCountry();
+    }
+
+    public static String getAccountId() {
+        if(account == null) initialize();
+
+        return account.getAccountId();
     }
 }

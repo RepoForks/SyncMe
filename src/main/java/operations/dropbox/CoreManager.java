5bcf635f70ee851327300fe6a4ffae87eb04fcaf
Adrián <arodriba@ull.edu.es>
2017-01-29 20:32:04 +0000

    Added Upload File operation

---
+++ src/main/java/operations/dropbox/CoreManager.java
@@ -0,0 +1,7 @@
+package operations.dropbox;
+
+/**
+ * Created by @AdrianBZG (www.adrianbazaga.com) on 29/01/2017.
+ */
+public class CoreManager {
+}
---
+++ src/main/java/operations/dropbox/UploadFile.java
@@ -0,0 +1,24 @@
+package operations.dropbox;
+
+import com.dropbox.core.v2.files.FileMetadata;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by @AdrianBZG (www.adrianbazaga.com) on 29/01/2017.
+ */
+public class UploadFile {
+    public void uploadFile(String path) {
+        // Upload to Dropbox
+        try (InputStream in = new FileInputStream(path)) {
+            FileMetadata metadata = CoreManager.getClient().files().uploadBuilder("/test.txt").uploadAndFinish(in);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
